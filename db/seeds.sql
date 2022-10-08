67c315e2277285f1b6f23fc7cbd5a1df4a6d0c15
AndrewKamSki <104537278+AndrewKamSki@users.noreply.github.com>
2022-10-08 17:13:06 -0400

    Added employee data to seed file

--- db/seeds.sql
+++ db/seeds.sql
@@ -14,19 +14,32 @@ VALUES
   ("Salesperson", 60000, 2),
   ("CEO", 350000, 3),
   ("CFO", 300000, 3),
-  ("COO", 300000, 3);
-  ("Representative", 42000, 4),
+  ("COO", 300000, 3),
   ("Supervisor", 65000, 4),
+  ("Representative", 42000, 4),
   ("Director", 150000, 5),
   ("Recruiter", 60000, 5),
-  ("Specialist", 63000, 5)
+  ("Specialist", 63000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-  ("The Great Gatsby", true, 1),
-  ("Huckleberry Finn", true, 3),
-  ("100 Years of Solitude", false, 5),
-  ("Things Fall Apart", false, 1),
-  ("Crime and Punishment", true, 2),
-  ("Moby Dick", true, 4),
-  ("Decameron", false, 1);
+  ("Evan", "Kane", 1, 3),
+  ("Tommy-Lee", "Allman", 1, 3),
+  ("Bernard", "Mclean", 2, NULL),
+  ("Hamza", "Rankin", 3, NULL),
+  ("Usmaan", "O'Conner", 3, NULL),
+  ("Elmer", "Duran", 4, 4),
+  ("Anisa", "Mora", 4, 4),
+  ("Zidane", "Power", 4, 5),
+  ("Sienna", "Henry", 5, NULL),
+  ("Lacey-May", "Healy", 6, NULL),
+  ("Stewart", "Cross", 7, NULL),
+  ("Ffion", "Green", 8, NULL),
+  ("Aaliyah", "Whitfield", 9, 12),
+  ("Cherise", "Blackmore", 9, 12),
+  ("Abiha", "Ayala", 9, 12),
+  ("Hayleigh", "Bravo", 10, NULL),
+  ("Jeremiah", "Bryan", 11, 16),
+  ("Shannon", "Burgess", 11, 16),
+  ("Cassia", "Obrien", 12, 16),
+  ("Rafe", "Millington", 12, 16);

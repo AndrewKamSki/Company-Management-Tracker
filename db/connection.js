ab697f99869077db8c9d152c082fb1a23c73d827
AndrewKamSki <104537278+AndrewKamSki@users.noreply.github.com>
2022-10-11 19:05:28 -0400

    Added initial prompt in menu

--- db/connection.js
+++ db/connection.js
@@ -2,15 +2,31 @@ const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
   host: "localhost",
-  // Your username
-  user: "<user>",
-  // Your password
-  password: "<password>",
-  database: "employees"
+  user: "root",
+  password: "password",
+  database: "company_db"
 });
 
 connection.connect(function (err) {
   if (err) throw err;
 });
+
+/*
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  }
+);
+
+module.exports = sequelize;
+*/
 
 module.exports = connection;
--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const { prompt } = require("inquirer");
-const db = require("./db");
-require("console.table");
+const db = require("./db/connection");
+const sql = require("mysql2/promise");
+const express = require("express");
+const table = require("console.table");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 init();
 
 // Display logo text, load main prompts
 function init() {
+  prompt({
+    type: 'list',
+    name: 'menu_options',
+    message: 'What would you like to do?',
+    choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Deparment', 'Quit']
+  })
   // loadPrompts();
 }
 
